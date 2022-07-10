e12e2dd221186f022a5d85f489f049b9b4e49267
root-rvl <91834746+root-rvl@users.noreply.github.com>
2022-07-10 18:00:11 +0530

    Update fcfs.py

--- fcfs.py
+++ fcfs.py
@@ -1,17 +1,23 @@
-at=[5,1,4,2,3,3] #arrival time
+at=[5,0,4,2,3,3] #arrival time
 at1=at.copy() #copy of arrival time
-bt=[2,1,8,4,5,6] #burst tme
+bt=[2,1,8,4,5,6] #burst time
 n=len(at) #Number of processes
-gantt_time=[min(at)] #[1,2,6,11,17,25,27]
-gantt_process=[] #[2,4,5,6,3,1]
+gantt_time=[min(at)]
+gantt_process=[]
 for i in range(n):
-    min_index=at.index(min(at)) #finding index of smallenst element of at.. smallest element=3.. index=5... process=index+1=5
-    gantt_process.append(min_index+1) #[2]
-    at[min_index]=float('inf') #[infinity,infinity,infinty,infinity,infinity,infinity]
+    min_index=at.index(min(at)) #finding index of smallenst element of at
+    gantt_process.append(min_index+1) 
+    at[min_index]=float('inf')
 for i in range(n):
-    gantt_time.append(gantt_time[-1]+bt[gantt_process[i]-1])
+    process=gantt_process[i] #current process
+    y=bt[process-1] #burst time of current process
+    #if no process at time
+    if at1[process-1]>gantt_time[-1]:
+        y+=at1[process-1]-gantt_time[-1]
+    gantt_time.append(gantt_time[-1]+y)
 tat_total=0
 wt_total=0
+print(gantt_process,gantt_time)
 gantt_time.pop(0)
 gantt=dict(zip(gantt_process,gantt_time)) #key=process no, values=CT
 print(gantt)
